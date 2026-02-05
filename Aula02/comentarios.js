63955bc1db5f2741da68ea618cfbaafc2dc06ba2
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-02-04 22:11:05 -0300

    Comentario
    Explicando que comentarios são ignorados

---
+++ Aula02/comentarios.js
@@ -0,0 +1,6 @@
+// Aprendendo sobre comentario
+console.log('Olá mundo!'); // Aqui tem outro comentario
+console.log('Linha 1');
+console.log('Linha 2');
+console.log('Linha 3');
+// Vai ser ignorado

69c2955d47f9048d51e4a6c134722f5763ee1371
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-02-04 22:14:50 -0300

    Explicando comentario longo

--- Aula02/comentarios.js
+++ Aula02/comentarios.js
@@ -1,6 +1,8 @@
 // Aprendendo sobre comentario
 console.log('Olá mundo!'); // Aqui tem outro comentario
 console.log('Linha 1');
+/*comentario longo
+linha comentado longo */
 console.log('Linha 2');
 console.log('Linha 3');
 // Vai ser ignorado

d1ca6a0d5c4a0b51be6ba9b5eff33a7ce606a6f4
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-02-06 16:48:16 -0300

    Treinando const

--- Aula04/contantes.js
+++ Aula04/contantes.js
@@ -2,4 +2,7 @@ const nome = 'Joana';
 const primeiroNumero = 5;
 const segundoNumero = 10;
 const conta = primeiroNumero * segundoNumero;
-console.log(conta);
+const resultadoDuplicado =  conta * 2;
+let resultadoTriplicado = conta * 3 ;
+resultadoTriplicado = resultadoTriplicado + 5
+console.log('Resultado : ',conta, '\nresultado duplicado : ',resultadoDuplicado, '\nResultado triplicado + 5 : ', resultadoTriplicado);

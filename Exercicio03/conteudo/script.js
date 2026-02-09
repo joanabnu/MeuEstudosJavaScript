a744d928b2086c799a406abf8cfcdef0ff3d6e48
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-02-09 16:47:31 -0300

    Exercicio de prompt

--- Exercicio03/conteudo/script.js
+++ Exercicio03/conteudo/script.js
@@ -1,4 +1,5 @@
 let  num1 = (parseInt(prompt('Digite Primeiro numero : ')));
 let num2 = parseInt(prompt('Digite Segundo numero : '));
 soma = (num1 + num2);
-alert(`o resultado é : ${soma}`);
+alert(`o Resultado é : ${soma}`);
+console.log(`O Resultado é : ${soma}`)

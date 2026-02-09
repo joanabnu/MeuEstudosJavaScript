0fee940b16e22271d434d9ce3101aa7f7cf7f453
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-02-09 11:15:51 -0300

    dados primitivos

---
+++ Aula04/dadosprimitivos.js
@@ -0,0 +1,9 @@
+const nome = 'joana'; //String
+const nome1 = "Maria"; // String
+const nome2 = `Joana`; //String
+const num1 = 10; // Number
+const num2 = 10.52 //Number
+let nomeAluno // undefined = Nao aponta para local nenhum na memoria. 
+let sobrenomeAluno = null // nulo nao aponta para local nenhuma na memoria.
+
+

400e6de568528ba1141429d29b736351c4c110f8
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-02-11 16:56:02 -0300

    numbers

---
+++ Aula08/numbers.js
@@ -0,0 +1,8 @@
+let num1 = 10.5899895; // number 
+let num2 = 2.5; // number
+
+console.log(num1.toString() + num2);
+console.log(typeof num1);
+console.log(num1.toString(2));
+console.log(num1.toFixed(4));
+console.log(Number.isInteger(num1)); // verificar se numero inteiro 

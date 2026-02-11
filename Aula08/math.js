900d82f1868b8b2c87b4ff25d019fcf62569ab9d
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-02-11 17:50:18 -0300

    Math

--- Aula08/math.js
+++ Aula08/math.js
@@ -3,4 +3,4 @@ let num1 = 9.54578;
 let numBaixo = Math.floor(num1); // arredondado para Baixo 
 let numCima = Math.ceil(num1) //  arredondado para cima 
 let maisProximo = Math.round(num1)
-console.log(`Numero arredondado para baixo : ${numBaixo} \nNumero arredondado para cima : ${numCima} \nNumero arredondado para mais proximo : ${maisProximo} \nO maior numero para sequencia é: ${Math.max(1,2,3,4,5, -10, -50, 1500,9,8,7,6)} \nO menor numero da sequencia é : ${Math.min(1,2,3,5,-10,-50,1500,9,8,7,6)} \nGerar numero aleatorio : ${Math.random()}`);
+console.log(`Numero arredondado para baixo : ${numBaixo} \nNumero arredondado para cima : ${numCima} \nNumero arredondado para mais proximo : ${maisProximo} \nO maior numero para sequencia é: ${Math.max(1,2,3,4,5, -10, -50, 1500,9,8,7,6)} \nO menor numero da sequencia é : ${Math.min(1,2,3,5,-10,-50,1500,9,8,7,6)} \nGerar numero aleatorio : ${Math.random()} \nValor de PI : ${Math.PI} \nEleva o numero : ${Math.pow(2,10)}`);

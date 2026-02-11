6d10bfa2280766bbfcef7321164c7d797547bb27
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-02-11 15:36:52 -0300

    string

---
+++ Aula07/string.js
@@ -0,0 +1,15 @@
+
+let umastring = 'Uma texto';
+console.log(umastring);
+console.log(umastring.match(/[a-z]/g));
+console.log(umastring.search(/x/));
+console.log(umastring.replace('Uma','outra'));
+
+let novaString = 'O rato roeu do rei roma.';
+console.log(novaString.replace(/r/g,'#'));
+console.log(novaString.length);
+console.log(novaString.slice(-3));
+console.log(novaString.slice(-5,-1));
+console.log(novaString.split('r'));
+console.log(novaString.split(' ',3));
+

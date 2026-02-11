e65b30fd05daab54d59b28d85446b361d8e580ba
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-02-11 16:30:37 -0300

    Exercicio string

--- Exercicio05/Javascript/script.js
+++ Exercicio05/Javascript/script.js
@@ -6,6 +6,7 @@ document.body.innerHTML += `Qual o primeiro indice da letra a no seu nome <stron
 document.body.innerHTML += `Qual o primeiro indice da letra ${nome.indexOf('a')}<br/>`;
 document.body.innerHTML += `O seu nome com letra minusculas : ${nome.toLocaleLowerCase()} <br/>`;
 document.body.innerHTML += `Qual o ultimo indice a no seu nome : ${nome.lastIndexOf('a')}<br/>`;
-document.body.innerHTML += `${nome.split('')} <br/>`;
+document.body.innerHTML += `As ultimas 3  letras do seu nome são : ${nome.slice(-3,nome.length)}<br/>`;
+document.body.innerHTML += `As palavras do seu nome são : ${nome.split('')} <br/>`;
 document.body.innerHTML += `O seu nome com letra Maiuculas : ${nome.toLocaleUpperCase()}<br/>`;
 console.log(`O seu nome é : ${nome}`);

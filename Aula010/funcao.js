b22e1329800c8bdefff75e379fcef38974333d4e
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-02-12 16:57:09 -0300

    funcao

--- Aula010/funcao.js
+++ Aula010/funcao.js
@@ -7,9 +7,20 @@ console.log(variavel);
 
 function soma(x,y){
     const resultado = x + y;
-    return resultado
-
+    return resultado;
 }
 console.log(soma(2,2));
 console.log(soma(3,1));
-console.log(soma(2,2))
+console.log(soma(2,7));
+
+function adicao(a= 1, b =1){
+    const total =  a + b;
+    return total;
+}
+const total = adicao(4,2);
+console.log(`Total : ${total}`)
+
+const raiz = function(n){
+    return n ** 0.5;
+}
+console.log(raiz(9));

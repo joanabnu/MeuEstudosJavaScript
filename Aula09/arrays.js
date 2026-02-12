a706f7494a6ab3f792df7590042786610667deed
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-02-12 16:21:19 -0300

    Arrays

--- Aula09/arrays.js
+++ Aula09/arrays.js
@@ -4,6 +4,6 @@ alunos.push('fabio'); //  adicionar no final
 alunos.unshift('Luiza'); // Adicionar no começo 
 const removido = alunos.pop();
 delete alunos[1];
-
+console.log(alunos instanceof Array)
 console.log(removido);
 console.log(alunos);

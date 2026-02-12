67c77bddadee8fb5abcdadf3dac182979678d1fc
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-02-12 16:39:47 -0300

    funcao soma

---
+++ Aula010/funcao.js
@@ -0,0 +1,15 @@
+function saudacao (nome) {
+ 
+ return (`Bom dia! ${nome} `)
+}
+const variavel = saudacao('Joana');
+console.log(variavel);
+
+function soma(x,y){
+    const resultado = x + y;
+    return resultado
+
+}
+console.log(soma(2,2));
+console.log(soma(3,1));
+console.log(soma(2,2))

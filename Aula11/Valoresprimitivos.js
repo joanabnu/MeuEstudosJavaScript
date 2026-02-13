6206fb27d4510c05f27c948ef3b6c0fc7b53b9df
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-02-13 18:40:04 -0300

    valores primitivos

---
+++ Aula11/Valoresprimitivos.js
@@ -0,0 +1,22 @@
+/* 
+Primitivos (imutaveis) -  String, number, boolean, undefined,null (bigint, symbol)
+Referencia (mutavel) - Array, objects, function - Passados por referencia
+*/
+let nome = 'Joana';
+nome = 'Maria';
+nome[0] = 'R';
+
+let a = 'A';
+let b = a // copia
+console.log(a,b)
+console.log(nome);
+
+const pessoa = {
+    nome: 'Luiz',
+    sobrenome: 'otavio'
+};
+const pessoa1 = pessoa;
+pessoa.nome = 'joao';
+console.log(pessoa1);
+
+

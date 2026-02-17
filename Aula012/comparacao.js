dc3320d020316ccc02d8ced8479975b319f24416
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-02-17 16:00:56 -0300

    comparacao

--- Aula012/comparacao.js
+++ Aula012/comparacao.js
@@ -13,6 +13,8 @@ Operadores de comparação
 const num1 = 10;
 const num2 = '10';
 const tipo = num1 == num2;
+const estrita = num1 === num2;
+console.log(estrita)
 console.log(tipo)
 const comp = 10 >= 5;
 console.log(10 > 5);

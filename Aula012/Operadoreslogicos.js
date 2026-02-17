b1fc2d5474248f03f10be01da07ee92102ec9041
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-02-17 16:29:20 -0300

    Operadores Logicos

---
+++ Aula012/Operadoreslogicos.js
@@ -0,0 +1,13 @@
+/* 
+Operadores Lógicos
+&& -> AND -> E Todas as expressão precisam ser verdadeiras para retornar True 
+|| -> OR -> OU 
+! -> NOT -> NÃO 
+*/
+
+const expressaoAnd = true && true && true;
+const expressaoOr =  false || false;
+
+
+
+

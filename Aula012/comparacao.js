9a8b020099208e015f56d351f36fc63d7025e7f8
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-02-17 15:56:28 -0300

    comparacao

---
+++ Aula012/comparacao.js
@@ -0,0 +1,18 @@
+/* 
+Operadores de comparação  
+ > maior que
+ >= maior ou igual
+ < menor que 
+ <= menor que ou igual a
+ ==  igualdade 
+ === igualdade estrita(alor e tipo)
+ != diferente
+ !== diferente estrito
+*/
+
+const num1 = 10;
+const num2 = '10';
+const tipo = num1 == num2;
+console.log(tipo)
+const comp = 10 >= 5;
+console.log(10 > 5);

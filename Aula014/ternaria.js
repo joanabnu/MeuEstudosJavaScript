cd3ef72f4889edd269ea1d5aec7a1456258d3672
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-04 15:56:46 -0300

    Operacao Ternaria

---
+++ Aula014/ternaria.js
@@ -0,0 +1,8 @@
+const pontuacaoUsuario = 999;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuario VIP' : 'Usuario normal';
+
+if (pontuacaoUsuario >= 1000){
+    console.log('Usuario VIP');
+} else{
+    console.log('Usuario normal');
+}

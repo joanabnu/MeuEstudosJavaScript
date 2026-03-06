1cca55d20e367e62ef9d1d926c88b21bc3323eee
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-06 15:41:56 -0300

    Estrutura de repeticao

--- Aula019/estruturaRepeticao.js
+++ Aula019/estruturaRepeticao.js
@@ -1,3 +1,16 @@
-for (let i = 1; i<=50;i++){
- console.log(`Linha : ${i}`)
+for (let i = 1; i<=10;i++){
+ console.log(`Linha : ${i}`);
+}
+console.log(`-=-`.repeat(20))
+console.log(`Nova Estrutura de Repetição`)
+console.log(`-=-`.repeat(20))
+for(let j = 10; j>=1;j--){
+    console.log(`Linha : ${j}`);
+}
+console.log(`-=-`.repeat(20));
+console.log(`Nova Estrutura de Repetição`)
+console.log(`-=-`.repeat(20))
+for (let g = 0; g <=10;g++){
+    const par = g % 2 === 0 ? 'par': 'impar';
+    console.log(g,par);
 }

f978ef78d2651a9ae93181c850327c51dbe67dab
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-06 14:56:54 -0300

    Atribuicao via desestruturacao

---
+++ Aula017/atribuicao.js
@@ -0,0 +1,16 @@
+// let a = 'A';
+// let b = 'B';
+// let c = 'C';
+
+// const numeros = [1,2,3];
+// [a,b,c] = numeros; 
+
+// [a,b,c] = [1,2,3];
+
+// const numeros = [1000, 2000, 3000, 4000 , 5000, 6000, 7000, 8000, 9000];
+// const [primeiroNumero,segundoNumero, ...resto] = numeros
+
+// console.log(primeiroNumero, segundoNumero,resto)
+
+const  numero = [[1,2,3], [4,5,6], [7,8,9]];
+console.log(numero[1][2]);

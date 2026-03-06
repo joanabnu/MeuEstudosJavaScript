9c4262d4eb3a92fc8bbdbf7af35a2f95574f2650
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2026-03-06 15:48:18 -0300

    Estrutura de Repeticao

--- Aula019/estruturaRepeticao.js
+++ Aula019/estruturaRepeticao.js
@@ -2,15 +2,24 @@ for (let i = 1; i<=10;i++){
  console.log(`Linha : ${i}`);
 }
 console.log(`-=-`.repeat(20))
-console.log(`Nova Estrutura de Repetição`)
-console.log(`-=-`.repeat(20))
+console.log(`Nova Estrutura de Repetição`);
+console.log(`-=-`.repeat(20));
 for(let j = 10; j>=1;j--){
     console.log(`Linha : ${j}`);
 }
 console.log(`-=-`.repeat(20));
-console.log(`Nova Estrutura de Repetição`)
-console.log(`-=-`.repeat(20))
+console.log(`Nova Estrutura de Repetição`);
+console.log(`-=-`.repeat(20));
 for (let g = 0; g <=10;g++){
     const par = g % 2 === 0 ? 'par': 'impar';
     console.log(g,par);
+}
+console.log(`-=-`.repeat(20));
+console.log(`Nova Estrutura de Repetição`)
+console.log(`-=-`.repeat(20));
+
+const frutas = ['Maça','Pera','Uva'];
+
+for (let f = 0; f< frutas.length; f++){
+console.log(`Indice : ${f} -`, frutas[f])
 }
